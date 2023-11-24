07625ee2cb0d069072e122af353f5d9ebb25564b
sen7 <minimalife7@gmail.com>
2023-11-24 15:40:58 +0900

    2023.11.24

--- src/js/script.js
+++ src/js/script.js
@@ -1,90 +1,66 @@
 jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
   // ハンバーガーメニュー
-  $(".js-hamburger").on("click", function () {
-    $(this).toggleClass("is-open");
-    if ($(this).hasClass("is-open")) {
-      openDrawer();
-    } else {
-      closeDrawer();
-    }
+  $(".js-hamburger,.js-drawer").click(function () {
+    $(".js-hamburger").toggleClass("is-open");
+    $(".js-drawer").fadeToggle();
+    $("body").toggleClass("active");
   });
 
-  // backgroundまたはページ内リンクをクリックで閉じる
-  $(".js-drawer a[href]").on("click", function () {
-    closeDrawer();
+  //ドロワーメニュー
+  $("#MenuButton").click(function () {
+    $(".js-drawer").toggleClass("is-open");
+    $(".drawer-menu").toggleClass("is-open");
+    $("html").toggleClass("is-fixed");
   });
 
-  $(window).on('resize', function () {
-    if (window.matchMedia("(min-width: 768px)").matches) {
-      closeDrawer();
-    }
-  });
-
-  // page-top
-  $('.js-page-top').on("click", function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 500);
-    return false;
-  });
-
-  function openDrawer() {
-    $(".js-drawer").fadeIn();
-    $(".js-hamburger").addClass("is-open");
-  }
-
-  function closeDrawer() {
-    $(".js-drawer").fadeOut();
-    $(".js-hamburger").removeClass("is-open");
-  }
-
-  // スワイパー（MV）
- const swiper1 = new Swiper('.js-mv-swiper', {
+  // mvスワイパー
+  const swiper1 = new Swiper(".js-mv-swiper", {
     loop: true,
     effect: "fade",
-    speed: 2000,
+    speed: 3000,
     allowTouchMove: false,
     autoplay: {
-      delay: 2000,
+      delay: 3000,
     },
   });
 
-  // スワイパー(campaign)
-  const swiper2 = new Swiper(".js-campaign__swiper", {
+  // campaignスワイパー
+  const swiper2 = new Swiper(".js-campaign-swiper", {
     spaceBetween: 24,
     loop: true,
     loopAdditionalSlides: 4,
     loopedSlides: 8,
     // maxBackfaceHiddenSlides:8,
     width: 280,
-    speed: 1500,
-    autoplay: {
-    disableOnInteraction: false,
-    },
+    speed: 3000,
+
+    // autoplay: {
+    //   disableOnInteraction: false,
+    // },
     allowTouchMove: true,
     breakpoints: {
-    768: {
-    spaceBetween: 40,
-    width: 333,
-  },
-},
+      768: {
+        spaceBetween: 40,
+        width: 333,
+      },
+    },
+
     navigation: {
-    nextEl: ".js-swiper-button-prev",
-    prevEl: ".js-swiper-button-next",
-  },
-});
-});
+      nextEl: ".js-swiper-button-prev",
+      prevEl: ".js-swiper-button-next",
+    },
+  });
 
-
- // ページトップ
+  // ページトップ
   const pageTop = $(".js-page-top");
   pageTop.hide();
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
-     // 100pxスクロールしたら発火
-     pageTop.fadeIn(); // 100px以上スクロールしたらボタンをフェードイン
+      // 100pxスクロールしたら発火
+      pageTop.fadeIn(); // 100px以上スクロールしたらボタンをフェードイン
     } else {
-     pageTop.fadeOut(); // 100px以下になったらボタンをフェードアウト
+      pageTop.fadeOut(); // 100px以下になったらボタンをフェードアウト
     }
   });
   pageTop.click(function () {
@@ -92,68 +68,74 @@ jQuery(function ($) {
       {
         scrollTop: 0,
       },
-     500 // 500ミリ秒かけてページトップに戻る
+      500 // 500ミリ秒かけてページトップに戻る
+    );
+    return false;
+  });
+
+  $(document).ready(function () {
+    let scrollHeight, scrollPosition, footHeight;
+    $("js-page-top").hide();
+    $(window).on("scroll", function () {
+      scrollHeight = $(document).height();
+      scrollPosition = $(window).height() + $(window).scrollTop();
+      footHeight = $("footer").innerHeight();
+
+      if (scrollHeight - scrollPosition <= footHeight) {
+        $("js-page-top").css({
+          position: "absolute",
+          bottom: footHeight + 20,
+        });
+      } else {
+        $("js-page-top").css({
+          position: "fixed",
+          bottom: "20px",
+        });
+      }
+    });
+
+    $("#page-top").click(function () {
+      $("body,html").animate(
+        {
+          scrollTop: 0,
+        },
+        400
       );
       return false;
     });
-
-$(document).ready(function () {
-  let scrollHeight, scrollPosition, footHeight;
-  $("#page-top").hide();
-  $(window).on("scroll", function () {
-    scrollHeight = $(document).height();
-    scrollPosition = $(window).height() + $(window).scrollTop();
-    footHeight = $("footer").innerHeight();
-
-    if (scrollHeight - scrollPosition <= footHeight) {
-      $("#page-top").css({
-        position: "absolute",
-        bottom: footHeight + 20,
-      });
-    } else {
-      $("#page-top").css({
-        position: "fixed",
-        bottom: "20px",
-      });
-    }
   });
 
-  $("#page-top").click(function () {
-    $("body,html").animate(
-      {
-        scrollTop: 0,
-      },
-      400
-      );
-      return false;
+  //イメージアニメーション
+  // スクロールして表示領域に入ったらclass付与
+  $(".js-fadeUp").on("inview", function () {
+    $(this).addClass("is-inview");
+  });
+
+  //要素の取得とスピードの設定
+  var box = $(".js-colorbox"),
+    speed = 500;
+
+  //.js-colorboxの付いた全ての要素に対して下記の処理を行う
+  box.each(function () {
+    $(this).append('<div class="color"></div>');
+    var color = $(this).find($(".color")),
+      image = $(this).find("img");
+    var counter = 0;
+
+    image.css("opacity", "0");
+    color.css("width", "0%");
+    //inviewを使って背景色が画面に現れたら処理をする
+    color.on("inview", function () {
+      if (counter == 0) {
+        $(this)
+          .delay(200)
+          .animate({ width: "100%" }, speed, function () {
+            image.css("opacity", "1");
+            $(this).css({ left: "0", right: "auto" });
+            $(this).animate({ width: "0%" }, speed);
+          });
+        counter = 1;
+      }
     });
   });
-
-
-//要素の取得とスピードの設定
-var box = $('.js-colorbox'),
-speed = 300;
-
-//.colorboxの付いた全ての要素に対して下記の処理を行う
-box.each(function(){
-$(this).append('<div class="color"></div>')
-var color = $(this).find($('.color')),
-image = $(this).find('img');
-var counter = 0;
-
-image.css('opacity','0');
-color.css('width','0%');
-//inviewを使って背景色が画面に現れたら処理をする
-color.on('inview', function(){
-  if(counter == 0) {
-    $(this)
-    .delay(400)
-    .animate({'width':'100%'},speed,function(){
-      image.css('opacity','1');
-      $(this).css({'left':'0' , 'right':'auto'});
-      $(this).animate({'width':'0%'},speed);
-    })
-    counter = 1;
-  }
-});
 });
